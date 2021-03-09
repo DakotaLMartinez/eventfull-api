a39d4adf721373bd2af40278d5a42a6b5d22f276
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-03-09 14:29:47 -0800

    add group_id to event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -1,4 +1,4 @@
 class EventSerializer
   include JSONAPI::Serializer
-  attributes :id, :name, :description, :start_time, :end_time, :location, :poster_url
+  attributes :id, :name, :description, :start_time, :end_time, :location, :poster_url, :group_id
 end
