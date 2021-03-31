5cb2984bb29252f0487fc0e7f12ccdf70e439d2f
CoderJay06 <jaycru06@yahoo.com>
2021-03-31 14:36:28 -0400

    Setup ActiveStorage and add Events

---
+++ db/migrate/20210331183301_create_events.rb
@@ -0,0 +1,14 @@
+class CreateEvents < ActiveRecord::Migration[6.1]
+  def change
+    create_table :events do |t|
+      t.string :name
+      t.datetime :start_time
+      t.datetime :end_time
+      t.string :location
+      t.references :group, null: false, foreign_key: true
+      t.references :users, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
